4940d2597488e79d315546a2f40b773ab70ae090
nahedkhamis-dev <nihedkhamis@gmail.com>
2024-07-27 00:50:24 +0300

    first day of kotlin learning

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun main (){
+    // this statment to print line
+    /* this statment to
+    print /* statment */
+    in new line */
+    print("hello world!")
+    println("hello world!")
+    println("hello world!")
+    println("hello")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    var firstName = "nahed"
+    firstName = ""
+    println(firstName)
+    println(firstName.isEmpty())
+
+
+    val number:Char = '1'
+    //number = 66
+    // 'Char class', "String class"
+    println(number)
+    println(number::class)
+
+    /*
+    integer types is:
+    Byte 8 bytes
+    Short 16 bytes
+    Int 32 bytes
+    Long 64 bytes
+
+    decimal types is:
+    Float 32 bytes
+    Double 64 bytes
+    */
+
+    // how to convert data type such as toLong()
+    // functions in kotlin suchas plus, minus, times, div
+
+    val num:Int = 5
+    val newNum: Int = num.minus(20)
+    println(newNum)
+
+    val str:String = "Nahed Khamis"
+    println(str.uppercase())
+    println(str.lowercase())
+
+    print("Hello World\nWelcome")
+
+    // trivial quotes used to write string in multi lines
+    val statement:String = """
+    Hello,
+    My name is nahed,
+    are you okey?
+    """
+    println(statement)
+
+    // to delete indentation use .trimIndent()
+    println(statement.trimIndent())
+
+    //to replace indentation use .replaceIndent()
+    println(statement.replaceIndent("- "))
+
+    //to delete character from statement use trimMargin()
+    val state:String = """
+    >> Hello,
+    >> My name is nahed,
+    >> are you okey?
+    """
+    println(state.trimMargin(">>"))
+    val name:String = "mohammed"
+    val age:Int = 24
+    val string:String = "My name is $name and age is $age and your name is ${name.length} characters long"
+    // or you can use concatenation as "my name is " + name + " and age is " + age
+    println(string)
+    println(name.length)
 }
